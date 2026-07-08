fa6b4d2f645443156005f36352022751d68ccbd3
Paul Burca <Paul.Burca@softwire.com>
2026-07-08 12:18:43 +0300

    Part 2: 1, 2

--- index.ts
+++ index.ts
@@ -1,14 +1,36 @@
 // This is our main function
 function fizzbuzz(): void {
     for (let i = 1; i <= 100; i++) {
+        let message:string = "";
+        let special:boolean = false;
+    
         if (i % 3 == 0) {
-            console.log("Fizz");
-            if (i % 5 == 0)
-               console.log("FizzBuzz"); 
+            special = true;
+            if (i % 5 == 0) {
+                message = "FizzBuzz";
+            } else {
+                message = "Fizz";
+            }
         } else if (i % 5 == 0) {
-            console.log("Buzz");
-        } else 
+            special = true;
+            message = "Buzz";
+        }
+
+        if (i % 7 == 0) {
+            special = true;
+            message = message.concat("Bang");
+        }
+
+        if (i % 11 == 0) {
+            special = true;
+            message = "Bong";
+        }
+
+        if (!special) {
             console.log(i);
+        } else {
+            console.log(message);
+        }
     }
 }
 // Now, we run the main function:
