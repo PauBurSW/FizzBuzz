ecdbbd43abe56cea6f73fb382a2b549aa326a27b
Paul Burca <Paul.Burca@softwire.com>
2026-07-08 12:03:50 +0300

    Part 1

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// This is our main function
+function fizzbuzz() {
+    console.log("Hello, World!");
+    // Put your code here...
+}
+// Now, we run the main function:
+fizzbuzz();
---
+++ index.ts
@@ -0,0 +1,15 @@
+// This is our main function
+function fizzbuzz(): void {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            console.log("Fizz");
+            if (i % 5 == 0)
+               console.log("FizzBuzz"); 
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+        } else 
+            console.log(i);
+    }
+}
+// Now, we run the main function:
+fizzbuzz();
