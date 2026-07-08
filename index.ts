064821e8f13689c57e3d52542a9e3b1b8a0afe5a
Paul Burca <Paul.Burca@softwire.com>
2026-07-08 13:34:12 +0300

    part2 3

--- index.ts
+++ index.ts
@@ -1,35 +1,43 @@
 // This is our main function
 function fizzbuzz(): void {
     for (let i = 1; i <= 100; i++) {
-        let message:string = "";
-        let special:boolean = false;
+        const messageList:string[] = [];
     
         if (i % 3 == 0) {
-            special = true;
-            if (i % 5 == 0) {
-                message = "FizzBuzz";
-            } else {
-                message = "Fizz";
-            }
-        } else if (i % 5 == 0) {
-            special = true;
-            message = "Buzz";
+            messageList.push("Fizz");
+        }
+        
+        if (i % 5 == 0) {
+            messageList.push("Buzz");
         }
 
         if (i % 7 == 0) {
-            special = true;
-            message = message.concat("Bang");
+            messageList.push("Bang");
         }
 
         if (i % 11 == 0) {
-            special = true;
-            message = "Bong";
+            messageList.length = 0;
+            messageList.push("Bong");
         }
 
-        if (!special) {
+        if (i % 13 == 0) {
+            for (let i = 0; i < messageList.length; i++) {
+                if (i == messageList.length - 1) {
+                    messageList.push("Fezz");
+                    break;
+                } else if (messageList[i + 1][1] == 'B') {
+                    messageList.splice(i + 1, 0, "Fezz");
+                    break;
+                }
+            }
+            if (messageList.length == 0)
+                messageList.push("Fezz");
+        }
+
+        if (!messageList.length) {
             console.log(i);
         } else {
-            console.log(message);
+            console.log(messageList.join(""));
         }
     }
 }
